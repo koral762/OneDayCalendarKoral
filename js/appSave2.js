ad0460fdb8fe0aed9e6eaff08c1c7460b5ce1d78
koral <koral762@gmail.com>
2022-03-24 16:47:10 +0200

    new

--- js/appSave2.js
+++ js/appSave2.js
@@ -5,10 +5,7 @@ const EVENTS = [
     { id: 2, start: 105, end: 135 }, // an event from 10:45am to 11:45am
     { id: 3, start: 120, end: 240 }, // an event from 11:00am to 1:00pm
     { id: 4, start: 180, end: 260 }, // an event from 12:00pm to 1:20pm
-    { id: 5, start: 180, end: 260 }, // an event from 12:00pm to 1:20pm
-    { id: 6, start: 500, end: 560 },// an event from 5:20pm to 6:20pm
-    { id: 7, start: 600, end: 660 },// an event from 5:20pm to 6:20pm
-    { id: 8, start: 650, end: 700 },// an event from 5:20pm to 6:20pm
+    { id: 5, start: 500, end: 560 },// an event from 5:20pm to 6:20pm
 ];
 
 const CONTAINERWIDTH = 500;
@@ -16,7 +13,6 @@ const CONTAINERWIDTH = 500;
 function creatEvent(arrEvents) {
 
     arrEvents.sort((a, b) => a.start - b.start);
-
     // debugger;
     arrEvents.forEach((event, eventIdx) => {
 
@@ -44,24 +40,22 @@ function creatEvent(arrEvents) {
 
             }
 
-            if (collidesWith.length > 1) {
+            if (collidesWith.length !== 0 && collidesWith.length !== 1) {
 
                 let width = parseInt(CONTAINERWIDTH / (collidesWith.length + 1));
-
+                
                 for (let x = 0; x <= collidesWith.length; x++) {
-
+                    
                     event.width = width;
-
+                    
                     arrEvents[eventIdx - x].width = width;
                     arrEvents[eventIdx - (collidesWith.length - x)].left = x * width;
-
+                    
                 }
             } else if (collidesWith.length === 1) {
 
-
-                arrEvents[collidesWith[0]].width = parseInt(arrEvents[collidesWith[0]].width / 2);
-                event.width = arrEvents[0].width
                 event.left = (arrEvents[collidesWith[0]].left !== 0) ? 0 : arrEvents[collidesWith[0]].width;
+                event.width = (arrEvents[collidesWith[0]].left === 0)? parseInt(arrEvents[0].width/2): CONTAINERWIDTH - arrEvents[0].width;
 
             } else {
                 event.left = 0;
@@ -86,8 +80,8 @@ function renderEvents(elementsToRender) {
     elementsToRender.forEach(obj => {
         strHtml += `<div class="event" style="top:${obj.start}px;
         height:${obj.end - obj.start}px;
-        width:${obj.width - 3}px;
-        left:${obj.left}px;
+        width:${obj.width -3}px;
+        left:${obj.left }px;
         ">New event<span>Location</span></div>`;
     })
 
@@ -99,19 +93,17 @@ function renderEvents(elementsToRender) {
 function renderHours() {
 
     var strHtml = '';
-    var ampm = '';
 
     for (let i = 9; i <= 21; i++) {
 
-        ampm = (i < 12) ? 'AM' : 'PM';
 
-        strHtml += `<p style="font-size:15px;">${i}:00<span style="margin-left:0.3rem">${ampm}</span></p>`;
-        if (i === 21) break;
-        strHtml += `<p style="font-size:10px">${i}:30 </p>`;
+        strHtml += `<p style="font-size:15px;">${i}:00</p>`;
+        if(i === 21) break;
+        strHtml += `<p style="font-size:10px">${i}:30</p>`;
 
     }
 
-
+    
 
     var elContainer = document.querySelector('.hours');
     elContainer.innerHTML = strHtml;
